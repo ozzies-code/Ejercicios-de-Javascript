20b5ebc2ce6409a2cd8ec02dd8877a88c9f5e001
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-10-12 12:26:52 -0400

    Use of continue2

---
+++ continuar2.js
@@ -0,0 +1,9 @@
+let text = "";
+
+loop1: for (let j = 1; j < 5; j++) 
+  loop2: for (let i = 1; i < 5; i++) {
+    if (i === 3) { continue loop1; }
+    text += i + "<br>";
+  }
+
+document.getElementById("demo").innerHTML = "<strong style='color: blue; font-style: bold; font-family: Arial;'>" + text + "</strong>";
