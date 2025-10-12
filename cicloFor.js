8a16554d653a86ef0a6fff9074dcdde94adeb987
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-10-12 07:50:20 -0400

    file modified

--- cicloFor.js
+++ cicloFor.js
@@ -1,8 +1,7 @@
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-
 let text = "";
 let i = 0;
-for (; i < cars.length; i++) 
-  text += cars[i] + "<br>";
+
+for (; i < 5; i++) 
+  text += "The number is " + i + "<br>";
 
 document.getElementById("demo").innerHTML = "<strong style='color: blue; font-style: bold; font-family: Arial;'>" + text + "</strong>";
